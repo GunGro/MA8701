e1a5a70eebe2cc00a70ee89e4865152860909680
TiniusM <60649745+TiniusM@users.noreply.github.com>
2021-04-23 14:24:05 +0200

    Done some XGboost stuff

--- Project3/04_xgb_model.R
+++ Project3/04_xgb_model.R
@@ -19,7 +19,7 @@ houses_raw <- read_excel("temp/houses.xlsx")
 houses <- houses_raw %>%
   select(id, sqm, expense, tot_price, lat, lng, kommune_name) %>%
   mutate(kommune_factor = fct_lump_n(kommune_name, 220) %>% as.integer()) 
-  
+
 ################### #
 # Split data using the rsample package ----
 # Set seed in order to make the analysis reproducible 
@@ -117,5 +117,42 @@ model_preds %>%
 # Write prediction data to excel ----
 ################### #
 
-write_xlsx(model_preds, "temp/predictions.xlsx")
+# write_xlsx(model_preds, "temp/predictions.xlsx")
+
+
+##### Tuning the model ######
+
+nfold = 5
+params = list(booster = "gbtree", objective = "binary:logistic", 
+               eta=0.3, gamma=0, max_depth=6, min_child_weight=1, 
+               subsample=1, colsample_bytree=1)
+
+xgbcv <- xgb.cv( params = params, data = train, nrounds = 100, 
+                 nfold = nfold, showsd = T, stratified = T, 
+                 print.every.n = 10, early.stop.round = 20, maximize = F)
+# Initialize values 
+
+trees = 30000
+grid = expand.grid(
+  shrinkage = c(0.001, 0.005, 0.01, 0.05), splits = c(1, 2, 3),
+  optimal_trees = 0, # To store results 
+  min_cv_err = 0) # To store results
+
+for (i in 1:dim(grid)[1]) { 
+  set.seed(123)
+  # Train the model
+  tune_model = gbm(totcst ~ ., distribution = "gaussian", 
+                   data = trans_train, 
+                   n.trees = trees,
+                   interaction.depth = grid$splits, 
+                   shrinkage = grid$shrinkage, 
+                   cv.folds = folds)
+  model = boost_tree(trees = 350) %>%
+    set_mode("regression") %>% 
+    set_engine("xgboost") %>% 
+    fit(tot_price ~ ., data = train)
+  
+  grid$optimal_trees[i] = which.min(tune_model$cv.error)
+  grid$min_cv_err[i] = min(tune_model$cv.error) 
+}
 
